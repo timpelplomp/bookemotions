e73125816c42d7e0b8a605d2d82bef1876f24b73
timpelplomp <timpelplomp@googlemail.com>
2019-05-17 23:35:37 +0200

    improved scaling

--- src/Canvas.py
+++ src/Canvas.py
@@ -6,26 +6,30 @@ from src.Text import Text
 
 class Canvas:
 
-    dimensions = None
     colour_space = "RGB"
-    background = "white"
+    background = 0
 
     # the folder where pictures reside in
     repository = "images"
+    dimensions = None
     image = None
     text = None
     drawer = None
     rec_height = 2
     scaled_colours = None
 
-    def __init__(self, text, dimensions=(200, 200)):
-        self.dimensions = dimensions
-        self.text = Text(text)
+    def __init__(self, text):
 
-        if len(self.text.colours) * self.rec_height > dimensions[1]:
-            self.scaled_colours = self.scaled_colours
-        else:
-            self.scaled_colours = text.colours
+        self.text = Text(text)
+        # x is 1 since we scale it later anyway - less memory
+        self.dimensions = (1, (len(self.text.colours)) * (self.rec_height + 1))
+
+        # scaling started
+        # if len(self.text.colours) * self.rec_height > dimensions[1]:
+        #     self.scaled_colours = self.scaled_colours
+        # else:
+        #     self.scaled_colours = self.text.colours
+        self.scaled_colours = self.text.colours
 
         self.image = Image.new(self.colour_space, self.dimensions, self.background)
         self.drawer = ImageDraw.Draw(self.image)
@@ -39,12 +43,18 @@ class Canvas:
                                   fill=colour)
             y_pos += self.rec_height + 1
 
-    def save(self, name):
+    def save(self, name, dimension=(200, 200)):
+        bbox = self.image.getbbox()
+        print(bbox)
+        print(self.image.size)
+        self.image = self.image.crop(bbox)
+        print(self.image.size)
+        self.image = self.image.resize(dimension)
         self.image.save(os.path.join(self.repository, name))
 
-    def __scale_colours(self):
-        scaled_colours = []
-        return scaled_colours
+    # def __scale_colours(self):
+    #     scaled_colours = []
+    #     return scaled_colours
 
 
 frame = Canvas("This is a beautiful day. I walk through the valley of death and despair. I hate this town.")
